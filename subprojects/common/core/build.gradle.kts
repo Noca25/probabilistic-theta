69dc21169a14be0aa3be4f3890c3fee2b183389a
Daniel Szekeres <szekeresdani@gmail.com>
2024-07-23 07:47:16 +0200

    build updates

--- subprojects/common/core/build.gradle.kts
+++ subprojects/common/core/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 
 dependencies {
     implementation(project(":theta-common"))
+    testImplementation(project(":theta-common"))
     val libPath: String by rootProject.extra
     testFixturesImplementation(fileTree(mapOf("dir" to libPath, "include" to listOf("*.jar"))))
     testFixturesImplementation(Deps.guava)
